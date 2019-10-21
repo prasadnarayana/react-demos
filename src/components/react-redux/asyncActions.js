d489c3aa8873c754d7fa7b48deb84b99e64b6cfd
prasadnarayana <prasadbabunarayana@gmail.com>
2019-10-21 23:56:58 +0530

    React middlewares, asyn actions demo

---
+++ src/components/react-redux/asyncActions.js
@@ -0,0 +1,86 @@
+const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+
+// State of app
+const initialState = {
+    loading: false,
+    users: [],
+    error: ""
+};
+
+// Action constants
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUEST
+    };
+};
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    };
+};
+
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    };
+};
+
+// Reducer function
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_USERS_REQUEST: return {
+            ...state,
+            loading: true
+        };
+
+        case FETCH_USERS_SUCCESS: return {
+            loading: false,
+            users: action.payload,
+            error: ""
+        };
+
+        case FETCH_USERS_FAILURE: return {
+            loading: false,
+            users: [],
+            error: action.payload
+        };
+
+        default: return state;
+    }
+};
+
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+// Action creator
+const fetchUsers = () => {
+    return function(dispatch) {
+        dispatch(fetchUsersRequest());
+
+        axios.get(URL)
+            .then(response => {
+                // response.data is the array of users
+                const users = response.data;
+                dispatch(fetchUsersSuccess(users));
+            })
+            .catch(error => {
+                // error.message is the error description
+                dispatch(fetchUsersFailure(error.message));
+            });
+    };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {console.log(store.getState())});
+store.dispatch(fetchUsers());
--- src/components/react-redux/index.js
+++ src/components/react-redux/index.js
@@ -1,7 +1,14 @@
 const redux = require("redux");
-const createStore = redux.createStore
+const reduxLogger = require("redux-logger");
+
+const createStore = redux.createStore;
+// Importing redux method to combine multiple reducers
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
     return {
@@ -10,13 +17,44 @@ function buyCake() {
     };
 }
 
-// (previousState, action) => newState
-
-const initialState = {
-    numberOfCakes: 10
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    };
 }
 
-const reducer = (state = initialState, action) => {
+// (previousState, action) => newState
+
+// const initialState = {
+//     numberOfCakes: 10,
+//     numberOfIceCreams: 20
+// }
+
+const initialCakeState = {
+    numberOfCakes: 10
+};
+
+const initialIceCreamState = {
+    numberOfIceCreams: 20
+};
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case BUY_CAKE: return {
+//             ...state, // spread operator ...
+//             numberOfCakes: state.numberOfCakes - 1
+//         };
+
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numberOfIceCreams: state.numberOfIceCreams - 1
+//         };
+
+//         default: return state;
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ...state, // spread operator ...
@@ -27,10 +65,28 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numberOfIceCreams: state.numberOfIceCreams - 1
+        };
+
+        default: return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial State: ", store.getState());
-const unsubscribe = store.subscribe(() => console.log("Updated State: ", store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
